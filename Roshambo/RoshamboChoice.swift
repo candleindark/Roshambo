59b65b3105fa768a0970ccf8c1ce92ccdda0ebe8
Isaac To <isaac@Lucia.local>
2016-03-13 15:22:42 -0500

    Add RoshamboChoice

---
+++ Roshambo/RoshamboChoice.swift
@@ -0,0 +1,11 @@
+//
+//  RoshamboChoice.swift
+//  Roshambo
+//
+//  Created by Isaac To on 3/13/16.
+//  Copyright © 2016 Isaac To. All rights reserved.
+//
+
+enum RoshamboChoice {
+        case Rock, Paper, Scissors
+}
