ac10aa9547644d631cdb04004b567e0834cd165e
Isaac To <isaac@Lucia.local>
2016-03-13 15:33:59 -0500

    Add model elements to ResultViewController

--- Roshambo/ResultViewController.swift
+++ Roshambo/ResultViewController.swift
@@ -9,10 +9,14 @@
 import UIKit
 
 class ResultViewController: UIViewController {
-
+    
+    var userChoice: RoshamboChoice?
+    var computerChoice: RoshamboChoice?
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
     }
 }
